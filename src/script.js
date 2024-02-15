054e1dfd7d01128659068bafa0f3b041d30d34fb
Nathan van de Pieterman <nathan.vandepieterman@gmail.com>
2024-02-15 13:49:36 +0100

    .

--- src/script.js
+++ src/script.js
@@ -69,7 +69,13 @@ function createProductCard(product) {
     const card = document.createElement('div');
     card.classList.add('w-full', 'max-w-sm', 'relative', 'group');
 
-    card.appendChild(createImageElement(product));
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('relative');
+
+    imageContainer.appendChild(createImageElement(product));
+    imageContainer.appendChild(createDescriptionElement(product));
+
+    card.appendChild(imageContainer);
 
     const textContainer = document.createElement('div');
     textContainer.classList.add('px-5', 'pb-5');
@@ -80,15 +86,31 @@ function createProductCard(product) {
     priceContainer.classList.add('flex', 'items-center', 'justify-between');
 
     priceContainer.appendChild(createPriceElement(product));
-    priceContainer.appendChild(createAddToCartElement());
+    priceContainer.appendChild(createAddToCartElement(product));
 
     textContainer.appendChild(priceContainer);
-
-    card.appendChild(createDescriptionElement(product));
 
     card.appendChild(textContainer);
 
     return card;
+}
+
+let cart = [];
+
+function addToCart(product) {
+    cart.push(product);
+}
+
+function createAddToCartElement(product) {
+    const addToCartLink = document.createElement('a');
+    addToCartLink.href = '#';
+    addToCartLink.classList.add('text-white', 'bg-grey-500', 'hover:bg-blue-800', 'focus:ring-4', 'focus:outline-none', 'focus:ring-blue-300', 'font-medium', 'rounded-lg', 'text-sm', 'px-5', 'py-2.5', 'text-center', 'dark:bg-blue-600', 'dark:hover:bg-blue-700', 'dark:focus:ring-blue-800');
+    addToCartLink.textContent = 'Add to cart';
+    addToCartLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        addToCart(product);
+    });
+    return addToCartLink;
 }
 
 
