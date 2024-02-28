6aa8ad6139c85cb3a843391ddfb0ddba42a337a7
Nathan van de Pieterman <nathan.vandepieterman@gmail.com>
2024-02-28 10:39:24 +0100

    fetch updated

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+let products;
+
 fetch('product.json')
     .then(response => {
         if (!response.ok) {
@@ -8,6 +10,9 @@ fetch('product.json')
     .then(data => {
         // Check if the fetched data contains the "products" array
         if (data.hasOwnProperty('products') && Array.isArray(data.products)) {
+            // Sla de productgegevens op in de variabele
+            products = data.products;
+
             // Create product cards for each product in the "products" array
             const productCardsContainer = document.getElementById('product-cards');
             data.products.forEach(product => {
@@ -106,7 +111,7 @@ function createAddToCartElement(product) {
     addToCartLink.href = '#';
     addToCartLink.classList.add('text-white', 'bg-grey-500', 'hover:bg-blue-800', 'focus:ring-4', 'focus:outline-none', 'focus:ring-blue-300', 'font-medium', 'rounded-lg', 'text-sm', 'px-5', 'py-2.5', 'text-center', 'dark:bg-blue-600', 'dark:hover:bg-blue-700', 'dark:focus:ring-blue-800');
     addToCartLink.textContent = 'Add to cart';
-    addToCartLink.addEventListener('click', function(event) {
+    addToCartLink.addEventListener('click', function (event) {
         event.preventDefault();
         addToCart(product);
     });
